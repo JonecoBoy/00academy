93e72799899f3e5589ba6af939ce77521efbcd88
joneco <joneco@hotmail.com>
2022-09-02 00:05:07 -0300

    create e list funcionando

--- src/core/usecases/courses/create-course/create-course.usecase.ts
+++ src/core/usecases/courses/create-course/create-course.usecase.ts
@@ -1,44 +1,39 @@
 import "reflect-metadata";
 import { inject, injectable } from "inversify";
 import {
-  CriaCursoInterface,
-  CriaCursoUseCaseParams,
+  CreateCourseInterface,
+  CriaCourseUseCaseParams,
 } from "./create-course.interface";
 import { CourseEntity } from "@core/entities/course.entity";
 import TYPES from "../../../../types";
 
-import { CursoRepositoryInterface } from "../../../providers/courses-repository.interface";
+import { CourseRepositoryInterface } from "../../../providers/courses-repository.interface";
 
 @injectable()
-export class CriaCursoUseCase implements CriaCursoInterface {
-  private _cursoRepository: CursoRepositoryInterface;
+export class CriaCourseUseCase implements CreateCourseInterface {
+  private _CourseRepository: CourseRepositoryInterface;
 
   constructor(
-    @inject(TYPES.CursoRepositoryInterface)
-    cursoRepository: CursoRepositoryInterface
+    @inject(TYPES.CourseRepositoryInterface)
+    CourseRepository: CourseRepositoryInterface
   ) {
-    // super();
-    this._cursoRepository = cursoRepository;
+    this._CourseRepository = CourseRepository;
   }
 
-  execute(model: CriaCursoUseCaseParams): CourseEntity {
-    // todo: validar regras de negocio
-    // todo: 01 validar se ja existe curso com o nome informado
-    const cursoFromDb = this._cursoRepository.search({
-      descricao: model.descricao,
-    });
+  execute(model: CriaCourseUseCaseParams): CourseEntity {
+    
+    // const CourseFromDb = this._CourseRepository.search({
+    //   descricao: model.descricao,
+    // });
 
-    if (!cursoFromDb) throw new Error(``);
+    // if (!CourseFromDb) throw new Error(``);
 
-    // todo: 02 o inicio do curso não pode ser em uma data retroativa
 
-    // todo: salvar o curso no banco
-    const result = this._cursoRepository.create({
+    const result = this._CourseRepository.create({
       dataInicio: model.dataInicio,
       descricao: model.descricao,
     });
 
-    // todo: montar saída do use case
     return result;
   }
 }
